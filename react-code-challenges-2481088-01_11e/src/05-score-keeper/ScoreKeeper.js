23e8c968f5f65d2c043bf004886f8dd76b3f8bde
Masooma <masooma.zahedi1@gmail.com>
2022-09-14 17:42:52 -0500

    learn score-keeper

--- react-code-challenges-2481088-01_11e/src/05-score-keeper/ScoreKeeper.js
+++ react-code-challenges-2481088-01_11e/src/05-score-keeper/ScoreKeeper.js
@@ -1,17 +1,22 @@
 import { useEffect, useState } from 'react'
 
 export default function ScoreKeeper () {
-  const [score, setScore] = useState(parseInt(localStorage.getItem('score')) || 0)
-
-  useEffect(() => {
-    localStorage.setItem('score', score)
-  }, [score])
-
+  const [score, setScore] = useState(parseInt(localStorage.getItem("score")) || 0)
+  useEffect(()=>{
+    localStorage.setItem("score", score)
+  },[score])
+  const decreaseNum = ()=>{
+    if (score > 0){
+      setScore(prevScore=> prevScore - 1)
+    } else{
+      setScore(0)
+    }
+  }
   return (
     <div>
-      <h1>Your score is: {score}</h1>
-      <button onClick={() => setScore(prevScore => prevScore + 1)}>+</button>
-      <button onClick={() => setScore(prevScore => prevScore - 1)}>-</button>
+      <h1>Your score is: {score}</h1> 
+      <button onClick={()=>setScore(prevScore=>prevScore + 1)}>+</button>
+      <button onClick={decreaseNum}>-</button>
     </div>
   )
 }
--- react-code-challenges-2481088-01_11e/src/App.js
+++ react-code-challenges-2481088-01_11e/src/App.js
@@ -1,10 +1,12 @@
 import ShoppingCart from './11-shopping-cart/ShoppingCart'
 import DogPic from './04-dog-pics/DogPics'
+import ScoreKeeper from "./05-score-keeper/ScoreKeeper"
 function App () {
   return (
     <div className='App'>
       {/* <ShoppingCart /> */}
-      <DogPic/>
+      {/* <DogPic/> */}
+      <ScoreKeeper/>
     </div>
   )
 }
